ecc0f4ed5af101d91d6526c2cf879407cf14f9ba
Tomi Paul <tomipaul95@gmail.com>
2018-01-15 02:56:30 +0100

    chore: set up application server
    [Delivers #154107331]

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// HomeHandler handle GET request to the root endpoint
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode("Welcome to the WeKnow API")
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	// Routes consist of a path and a handler function.
+	r.HandleFunc("/", HomeHandler).
+		Methods("GET")
+
+	// Bind to a port and pass our router in
+	log.Fatal(http.ListenAndServe(":3000", r))
+}
